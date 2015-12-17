5e28c907706c4f32ca5b3316685632f3a6571372
samw7823 <srwiener@gmail.com>
2015-12-17 07:52:07 +0000

    Added files for tibet media project

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ website_info = None
 @app.route("/")
 def hello():
     website_info = get_website_info()
-    website_info['current'] = 'hello'
+    website_info['current'] = 'home'
     return render_template("hello.html", info=website_info)
 def get_website_info():
     global website_info
@@ -21,11 +21,16 @@ def get_website_info():
             website_info = json.loads(f.read())
     return website_info
 
-@app.route("/hello")
+@app.route("/home")
 def home():
     website_info = get_website_info()
-    website_info['current'] = 'hello'
+    website_info['current'] = 'home'
     return render_template("hello.html", info=website_info)
+@app.route("/media")
+def media():
+    website_info = get_website_info()
+    website_info['current'] = 'media'
+    return render_template("media.html", info=website_info)
 @app.route("/maps")
 def map():
     website_info = get_website_info()
@@ -33,16 +38,25 @@ def map():
     return render_template("maps.html", info=website_info)
 
 @app.route("/languages")
-def lang():
+def languages():
     website_info = get_website_info()
     website_info['current'] = 'languages'
-    return render_template("languages.html", info=website_info)
+    return render_template("language.html", info=website_info)
 
-@app.route("/analysis")
-def analysis():
+@app.route("/celebrities")
+def celebs():
     website_info = get_website_info()
-    website_info['current'] = 'analysis'
-    return render_template("analysis.html", info=website_info)
+    website_info['current'] = 'celebrities'
+    return render_template("celebrities.html", info=website_info)
+@app.route("/sources")
+def sources():
+    website_info = get_website_info()
+    website_info['current'] = 'sources'
+    return render_template("sources.html", info=website_info)
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 
